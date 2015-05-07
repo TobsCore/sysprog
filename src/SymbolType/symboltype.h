f1bf013b1fdf326856a29f9a8dccb21a058cf355
Maike Rees <maikefer_@gmx.de>
2015-05-07 15:58:56 +0200

    added sign-type enum and namespace

---
+++ src/SymbolType/symboltype.h
@@ -0,0 +1,24 @@
+namespace sign
+{
+    enum signtype
+    {
+    	PLUS = 3,				// +
+		MINUS = 4,				// -
+		COLON = 5,				// :
+		STAR = 6,				// *
+		LESS = 7, 				// <
+		GREATER = 8, 			// >
+		EQUALS = 9,				// =
+		ASSIGN = 10,		 	// :=
+		SPECIAL = 11, 			// <:>
+		EXCLAMATION = 12,		// !
+		AND = 13,				// &
+		SEMICOLON = 14,			// ;
+		PARANTHESES_LEFT = 15, 	// (
+		PARANTHESES_RIGHT = 16, // )
+		BRACES_LEFT  = 17, 		// {
+		BRACES_RIGHT = 18, 		// }
+		BRACKET_LEFT = 19, 		// [
+		BRACKET_RIGHT = 20 		// ]
+    };
+}
