7522681f03af12269a0ca70bf0664d97e24ddb74
Tobias Kerst <tobias.kerst@tobscore.com>
2015-05-08 02:00:35 +0200

    Improves tests for automat

--- src/Scanner/Automat/automatTest.cpp
+++ src/Scanner/Automat/automatTest.cpp
@@ -2,12 +2,38 @@
 #include <iostream>
 using namespace std;
 
+void simpleTest();
+void testIdentifierAddition();
 void checkReturnValue(int returnValue, int expectedValue);
 
 
 int main () {
+    
+    simpleTest();
+    testIdentifierAddition();
+	return 0;
+}
 
-	Automat* testAutomat = new Automat();
+void simpleTest() {
+    
+    cout << "Test Integer Addition Addition:" << endl;
+    Automat* testAutomat = new Automat();
+    
+    checkReturnValue(testAutomat->checkExpression('1'), 0);
+    checkReturnValue(testAutomat->checkExpression('2'), 0);
+    checkReturnValue(testAutomat->checkExpression('3'), 0);
+    checkReturnValue(testAutomat->checkExpression('4'), 0);
+    checkReturnValue(testAutomat->checkExpression('+'), 1); //Is an integer
+    checkReturnValue(testAutomat->checkExpression('5'), 3); //Is a plus sign
+    checkReturnValue(testAutomat->checkExpression('2'), 0);
+    checkReturnValue(testAutomat->checkExpression(';'), 1); //Is an integer
+
+}
+
+void testIdentifierAddition() {
+    
+    cout << endl << "Test Identifier Addition:" << endl;
+    Automat* testAutomat = new Automat();
     
     checkReturnValue(testAutomat->checkExpression('a'), 0);
     checkReturnValue(testAutomat->checkExpression('a'), 0);
@@ -15,8 +41,9 @@ int main () {
     checkReturnValue(testAutomat->checkExpression('b'), 0);
     checkReturnValue(testAutomat->checkExpression('+'), 2); //Is an identifier
     checkReturnValue(testAutomat->checkExpression('a'), 3); //Is a plus sign
-	return 0;
 }
+
+
 
 void checkReturnValue(int returnValue, int expectedValue) {
     if(returnValue == expectedValue) {
