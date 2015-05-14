df365f89bd4ec922311123b4971ff0778bb49a74
Tobias Kerst <tobias.kerst@tobscore.com>
2015-05-15 01:18:46 +0200

    Enables the buffer to read the contents of a given file and then process it. Alternativeley it will just read the testProgram.txt file.

--- src/Scanner/Buffer/main.cpp
+++ src/Scanner/Buffer/main.cpp
@@ -10,11 +10,20 @@
 
 using namespace std;
 
-int main() {
-	char* filePath = "./test.txt";
+int main(int argc, char** argv) {
+
+	// Either get a path via arguments, or just simply use the test.txt
+	char* filePath;
+	if(argc == 2) {
+		filePath = (char*)argv[1];
+	} else {
+		filePath = "./testProgramm.txt";
+	}
+
+	cout << filePath << endl;
 
 	Buffer* joo = new Buffer(filePath);
-	for (int i = 0; i< 1600; i++)
+	for (int i = 0; i< 128; i++)
 	{
 	char c = joo->getChar();
 	cout << "mein char: " << i << ": " << c << endl;
