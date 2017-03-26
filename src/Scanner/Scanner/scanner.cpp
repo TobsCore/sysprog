678b66cb80017295a4fe2d788268ae738a626892
Tobias Kerst <tobias.kerst@tobscore.com>
2017-03-26 09:15:32 +0200

    Tokenizer Loop works now

--- src/Scanner/Scanner/scanner.cpp
+++ src/Scanner/Scanner/scanner.cpp
@@ -22,6 +22,7 @@ Scanner::Scanner(char* filePath) {
 Token Scanner::nextToken() {
 	Token* nextToken = new Token();
 
+	// If no other characters can be read, return and EOF-Token. This can be used to stop the calling loop.
 	if (!buffer->hasNext()) {
 		nextToken->setType(FILE_END);
 		return *nextToken;
@@ -31,11 +32,17 @@ Token Scanner::nextToken() {
 	do {
 		char nextChar = buffer->getChar();
 		symbol = automat->checkExpression(nextChar);
-	} while(symbol == NEXTCHAR);
+	} while (symbol == NEXTCHAR && buffer->hasNext());
+
+	// If a symbol has a NEXTCHAR, this means that the loop has quit because there's no other character in the buffer.
+	// This equals to an EOF, which should be returned.a
+	if (symbol == NEXTCHAR) {
+		symbol = FILE_END;
+	}
+
 	nextToken->setType(symbol);
 	return *nextToken;
 }
-
 
 int main(int argc, char** argv) {
 	if (argc != 2) {
