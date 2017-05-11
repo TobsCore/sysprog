320fc367e09975d3d129e574b799b9617c340ded
Tobias Kerst <tobias.kerst@tobscore.com>
2017-05-11 10:27:22 +0200

    Writes output to file

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,17 +1,38 @@
+#include <fstream>
 #include "Scanner/Scanner/Scanner.h"
 
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        cout << "Missing argument." << endl;
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        cerr << "Missing arguments." << endl;
     } else {
-        char *filePath = argv[1];
-        Scanner *sc = new Scanner(filePath);
+        const char *inFile = argv[1];
+        const char *outFilename = argv[2];
+        Scanner *sc = new Scanner(inFile);
 
-        Token* token = sc->nextToken();
+
+        // Clear output file
+        ofstream ofs;
+        ofs.open(outFilename, std::ofstream::out | std::ofstream::trunc);
+        ofs.close();
+
+        // Creates file appender for output file
+        ofstream result(outFilename, std::ios_base::app);
+        cout << "Running..." << endl;
+
+        Token *token = sc->nextToken();
         while (!token->isEOF()) {
-            cout << token->toString() << endl;
+            if (result.is_open()) {
+                result << token->toString() << endl;
+            } else {
+                cerr << "Cannot write to file <" << outFilename << ">" << endl;
+                break;
+            }
+
             token = sc->nextToken();
         }
+
+        cout << "Finished! Output written to <" << outFilename << ">" << endl;
+        result.close();
     }
     return 0;
 }
