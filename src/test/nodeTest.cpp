e98a097fd2c6512e0ce70750d4d0e55deae85f35
Tobias Kerst <tobias.kerst@googlemail.com>
2017-06-12 17:14:51 +0200

    Improves node testing

--- src/test/nodeTest.cpp
+++ src/test/nodeTest.cpp
@@ -2,35 +2,47 @@
 #include <gmock/gmock.h>
 #include "../main/Parser/Parser/Node.h"
 
-TEST(NodeTest, AddNoNode) {
-    Node *node = new Node();
-    ASSERT_THROW(node->getChild(0), std::out_of_range);
-    ASSERT_THROW(node->getChild(1), std::out_of_range);
+using testing::Eq;
+
+namespace {
+    class NodeTest : public testing::Test {
+    public:
+        Node node;
+    };
 }
 
-TEST(NodeTest, AddOneNode) {
-    Node *node = new Node();
+TEST_F(NodeTest, AddNoNode) {
+    ASSERT_THROW(node.getChild(0), std::out_of_range);
+    ASSERT_THROW(node.getChild(1), std::out_of_range);
+}
+
+TEST_F(NodeTest, AddOneNode) {
     Node *insertNode = new Node();
 
-    node->addChild(insertNode);
-    ASSERT_EQ(insertNode, node->getChild(0));
+    node.addChild(insertNode);
+    ASSERT_EQ(insertNode, node.getChild(0));
 }
 
-TEST(NodeTest, AddMaxNodes) {
-    Node *node = new Node();
-
+TEST_F(NodeTest, AddMaxNodes) {
     for (int i = 0; i < UCHAR_MAX; i++) {
         Node *insertNode = new Node();
-        node->addChild(insertNode);
-        ASSERT_EQ(insertNode, node->getChild(i));
+        node.addChild(insertNode);
+        ASSERT_EQ(insertNode, node.getChild(i));
     }
 }
 
-TEST(NodeTest, AddMaxNodesPLUSONE) {
-    Node *node = new Node();
-
+TEST_F(NodeTest, AddMaxNodesPLUSONE) {
     for (int i = 0; i < UCHAR_MAX; i++) {
-        node->addChild(new Node());
+        node.addChild(new Node());
     }
-    ASSERT_THROW(node->addChild(new Node), std::out_of_range);
+    ASSERT_THROW(node.addChild(new Node), std::out_of_range);
+}
+
+TEST_F(NodeTest, IsLeafForNewlyCreatedNode) {
+    ASSERT_TRUE(node.isLeaf());
+}
+
+TEST_F(NodeTest, IsLeafAfterAddingChil) {
+    node.addChild(new Node());
+    ASSERT_FALSE(node.isLeaf());
 }
