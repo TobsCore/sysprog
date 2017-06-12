e99e5d346dc1414e12e06e02642500aa2ee2d6ce
Tobias Kerst <tobias.kerst@googlemail.com>
2017-06-12 15:13:06 +0200

    Improves color coding by using colors.h

--- src/main/Scanner/runScanner.cpp
+++ src/main/Scanner/runScanner.cpp
@@ -1,7 +1,8 @@
 #include <fstream>
 #include "Scanner/Scanner.h"
+#include "../colors.h"
 
-void clearOutputFile(const char* outFilename) {
+void clearOutputFile(const char *outFilename) {
     ofstream ofs;
     ofs.open(outFilename, std::ofstream::out | std::ofstream::trunc);
     if (!ofs.is_open()) {
@@ -33,13 +34,13 @@ int main(int argc, char **argv) {
         Token *token = sc->nextToken();
         while (!token->isEOF()) {
             if (token->getType() == ERROR) {
-                cerr << "\033[1;31m" << token->toString() << "\033[0m" << endl;
+                cerr << RED << token->toString() << COLOR_RESET << endl;
             } else {
                 if (result.is_open()) {
                     result << token->toString() << endl;
                 } else {
-                    cerr << "\033[1;31m" << "Error!" << "\033[0m" << " Cannot write to output file <" << outFilename <<
-                                                                                                                   ">"
+                    cerr << RED << "Error!" << COLOR_RESET << " Cannot write to output file <" << outFilename <<
+                         ">"
                          << endl;
                     exit(EXIT_FAILURE);
                 }
@@ -48,12 +49,12 @@ int main(int argc, char **argv) {
             token = sc->nextToken();
         }
 
-        cout << "\033[1;32m" << "Finished!" << "\033[0m" << endl << "Output written to <" << outFilename << ">" <<
+        cout << GREEN << "Finished!" << COLOR_RESET << endl << "Output written to <" << outFilename << ">" <<
              endl;
         result.close();
 
     } catch (std::exception &ex) {
-        cerr << "\033[1;31m" << "Error! " << "\033[0m" << "Cannot read input file <" << inFile << ">" << endl;
+        cerr << RED << "Error! " << COLOR_RESET << "Cannot read input file <" << inFile << ">" << endl;
         exit(EXIT_FAILURE);
 
     }
---
+++ src/main/colors.h
@@ -0,0 +1,16 @@
+//
+// Created by Tobias Kerst on 12.06.17.
+//
+
+#ifndef COLORS_H
+#define COLORS_H
+
+#define RED "\033[31;1m"
+#define GREEN "\033[32;1m"
+#define WHITE "\033[37;1m"
+#define BLUE "\033[34;1m"
+#define CYAN "\033[36;1m"
+#define ORANGE "\033[35;1m"
+#define COLOR_RESET "\033[0m"
+
+#endif
