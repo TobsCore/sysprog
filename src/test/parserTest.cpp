dc9e6be332cacb5b3c2b2a46d272b8e31d249539
Tobias Kerst <tobias.kerst@googlemail.com>
2017-06-19 00:05:47 +0200

    Improves Parser testing

--- src/test/parserTest.cpp
+++ src/test/parserTest.cpp
@@ -2,9 +2,30 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "../main/Parser/Parser/Parser.h"
+#include "../main/Parser/Parser/Grammar.h"
 
 
 TEST(ParserTest, ReadSimpleIntFile) {
-    Parser *parser = new Parser("../src/test/testData/IntSimple.txt", "testout.txt");
-    parser->parse();
+    Parser *parser = new Parser("../src/test/testData/programs/simplestProgram.txt", "testout.txt");
+    ParseTree *parseTree = parser->parse();
+
+    Node *root = parseTree->getTree();
+    ASSERT_EQ(root->getRuleType(), PROG);
+    ASSERT_EQ(root->getChild(0)->getRuleType(), DECLS);
+    ASSERT_EQ(root->getChild(1)->getRuleType(), STATEMENTS_EMPTY);
+
+    Node *decls = root->getChild(0);
+    ASSERT_EQ(decls->getChild(0)->getRuleType(), DECL);
+    ASSERT_EQ(decls->getChild(1)->getRuleType(), DECLS_EMPTY);
+
+    Node *decl = decls->getChild(0);
+    ASSERT_EQ(decl->getChild(0)->getRuleType(), ARRAY_EMPTY);
+}
+
+TEST(ParserTest, CheckSimplestWithGrammar) {
+    Parser *parser = new Parser("../src/test/testData/programs/simplestProgram.txt", "testout.txt");
+    ParseTree *parseTree = parser->parse();
+
+    Grammar *grammar = new Grammar();
+    //grammar->typeCheck(parseTree);
 }
